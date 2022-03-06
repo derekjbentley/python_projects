e197154ab51af6feb92afc53097395815fed1d20
derekjbentley <derek.j.bentley@gmail.com>
2022-03-06 16:14:22 -0500

    Added dynamic user location

--- 01_dc_weather/dc_weather.py
+++ 01_dc_weather/dc_weather.py
@@ -1,19 +1,32 @@
 # ! python
-# dc_weather.py - Prints the weather for Cleveland Park, Washington, DC
+# current_weather.py - Prints the current weather for the user's IP address
+'''
+current_weather.py retrieves the current weather conditions at the location of 
+the user's IP address. The data is retrived with the Open Weather Map API.
+'''
 
 ## Import modules
 import json
 import requests
 import os
+import geocoder
 from dotenv import load_dotenv
 
 ## Retrieve credentials and set environment variables
 load_dotenv()
 APPID = os.environ.get('openweather_key')
 
-## Cleveland Park Coordinates
-lat = '38.9346'
-lon = '-77.0664'
+## Retrieve User's Coordinates
+def user_location():
+    '''Return the user's latitude and longitude coordinates based on their IP address.'''
+    g = geocoder.ip('me')
+    coordinates = list(g.latlng)
+    lat, lon = coordinates
+    city = g.city
+    state = g.state
+    return lat, lon, city, state
+
+lat, lon, city, state = user_location()
 
 ## API Call
 url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={APPID}&units=imperial"
@@ -29,7 +42,15 @@ weatherData = json.loads(response.text)
 current = weatherData['current']
 
 ## Print current weather
-print('The current weather for Cleveland Park, Washington, DC:')
-print('Temperature: ' + str(current['temp']))
-print('Feels like: ' + str(current['feels_like']))
-print('Cloud Cover: ' + current['weather'][0]['description'].title())
+def output_weather():
+    '''Prints the current weather conditions for the user's location.'''
+    if state == 'Washington, D.C.':
+        print(f'The current weather for {state}:')
+    else:
+        print(f'The current weather for {city}, {state}:')
+
+    print('Temperature: ' + str(current['temp']))
+    print('Feels like: ' + str(current['feels_like']))
+    print('Cloud Cover: ' + current['weather'][0]['description'].title())
+
+output_weather()
